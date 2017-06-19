59a6b83e479f015f2220d1b5b8481d54169944bc
ancaciascaiu <anca.ciascaiu@gmail.com>
2017-06-19 13:20:32 -0500

    async await mongoose promise

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
 	res.render('index');
-}
+};
+
+exports.addStore = (req, res) => {
+	res.render('editStore', {title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+	const store = new Store(req.body);
+	store
+		.save()
+		.then(store => {
+			return Store.find()
+		})
+		.then(stores => {
+			res.render('storeList', {stores: stores})
+		})
+		.catch(err => {
+			throw Error(err);
+		})
+	console.log('It worked!');
+};
