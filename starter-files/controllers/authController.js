583b2e2c6babf9e33851c043bf29b2af7d414885
ancaciascaiu <anca.ciascaiu@gmail.com>
2017-06-22 16:55:20 -0500

    only allow logged in user to add new store

--- starter-files/controllers/authController.js
+++ starter-files/controllers/authController.js
@@ -13,10 +13,10 @@ exports.logout = (req, res) => {
 	res.redirect('/');
 }
 
+//for user authorization
 exports.isLoggedIn = (req, res, next) => {
-  // first check if the user is authenticated
   if (req.isAuthenticated()) {
-    next(); // carry on! They are logged in!
+    next();
     return;
   }
   req.flash('error', 'Oops you must be logged in to do that!');
