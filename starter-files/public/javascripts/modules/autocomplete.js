982bbbbb0525222ef78cf435c762efdc5bf6e458
ancaciascaiu <anca.ciascaiu@gmail.com>
2017-06-25 18:24:29 -0500

    search bar logic

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -6,12 +6,11 @@ function autocomplete(input, latInput, lngInput) {
 		const place = dropdown.getPlace();
 		latInput.value = place.geometry.location.lat();
 		lngInput.value = place.geometry.location.lng();
-
 	});
 	//if I hit eneter on the address field, don't submit the form
 	input.on('keydown', (e) => {
 		if(e.keyCode === 13) e.preventDefault();
-	})
+	});
 }
 
 export default autocomplete;
